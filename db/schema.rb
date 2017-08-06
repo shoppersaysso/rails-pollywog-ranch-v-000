c8f317fe11177737b4375f0f56b58e6858271772
Sara Hopper <sara.hopper@gmail.com>
2017-08-06 21:25:47 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,20 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140922151444) do
+ActiveRecord::Schema.define(version: 20140922151423) do
 
   create_table "frogs", force: :cascade do |t|
     t.string   "name"
     t.string   "color"
     t.integer  "pond_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "tadpoles", force: :cascade do |t|
-    t.string   "name"
-    t.string   "color"
-    t.integer  "frog_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
