0a153d8314ba99e58daba9772eac4036b6c42522
Sara Hopper <sara.hopper@gmail.com>
2017-08-06 21:26:27 +0000

    automatically backed up by learn

--- db/migrate/20140922151423_create_frogs.rb
+++ db/migrate/20140922151423_create_frogs.rb
@@ -3,7 +3,7 @@ class CreateFrogs < ActiveRecord::Migration
     create_table :frogs do |t|
       t.string :name
       t.string :color
-          t.belongs_to :pond, index:true, foreign_key:true
+      t.belongs_to :pond, index:true, foreign_key:true
 
       t.timestamps null: false
     end
