2f47b5b9e9c41ea7614028e81c9059376953d249
Sara Hopper <sara.hopper@gmail.com>
2017-08-06 20:28:50 +0000

    automatically backed up by learn

--- db/migrate/20140922151507_create_ponds.rb
+++ db/migrate/20140922151507_create_ponds.rb
@@ -1,3 +1,10 @@
 class CreatePonds < ActiveRecord::Migration
-  # code goes here
+  def change
+    create_table :pond do |t|
+      t.string :name
+      t.string :water_type
+
+      t.timestamps null: false
+    end
+  end
 end
