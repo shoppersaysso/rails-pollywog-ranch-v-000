885320c3723272295988216bacdcf5b04af6702e
Sara Hopper <sara.hopper@gmail.com>
2017-08-06 21:00:15 +0000

    automatically backed up by learn

--- db/migrate/20140922151444_create_tadpoles.rb
+++ db/migrate/20140922151444_create_tadpoles.rb
@@ -3,6 +3,7 @@ class CreateTadpoles < ActiveRecord::Migration
     create_table :tadpoles do |t|
       t.string :name
       t.string :color
+      t.integer :frog_id
 
       t.timestamps null: false
     end
